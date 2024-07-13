0f6e983b0374f4c5d0755ba4755170d8f5bd7133
thembinkosi <twala.emma@gmail.com>
2024-07-13 16:09:20 +0200

    changes
    connected with mongo

--- Loada-Backend/index.js
+++ Loada-Backend/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/routes/index.js
 const express = require('express');
 const router = express.Router();
@@ -10,16 +9,3 @@ router.use('/Collectors', require('./src/routes/collectorRoutes'));
 router.use('/Pickups', require('./src/routes/pickupRoutes'));
 
 module.exports = router;
-=======
-// src/routes/index.js
-const express = require('express');
-const router = express.Router();
-
-router.use('/users', require('./src/routes/userRoutes'));
-router.use('/bookings', require('./src/routes/bookingRoutes'));
-router.use('/Items', require('./src/routes/itemRoutes'));
-router.use('/Collectors', require('./src/routes/collectorRoutes'));
-router.use('/Pickups', require('./src/routes/pickupRoutes'));
-
-module.exports = router;
->>>>>>> a48951c7044dcced6f8342e8bd78cfd81b9819db
